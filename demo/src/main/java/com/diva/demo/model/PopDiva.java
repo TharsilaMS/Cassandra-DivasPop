aa6ec4ec0ba24a271888c35c7e319985a5900d3f
TharsilaMS <tharsilams@gmail.com>
2024-09-02 19:36:02 -0300

    Não conecta no banco

--- demo/src/main/java/com/diva/demo/model/PopDiva.java
+++ demo/src/main/java/com/diva/demo/model/PopDiva.java
@@ -1,6 +1,5 @@
 package com.diva.demo.model;
 
-
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 import org.springframework.data.cassandra.core.mapping.Column;
